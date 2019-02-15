f3c813b3286f102db30f702dae16b47346149142
siyabd <42607735+siyabd@users.noreply.github.com>
2019-02-15 13:27:21 +0200

    Update config.php

--- core/config.php
+++ core/config.php
@@ -3,7 +3,7 @@ return[
 	'name' => 'mechanic',
 	'username' => 'siyabd',
 	'password' => '1234',
-	'connection' => 'mysql://mysql:3306'',
+	'connection' => 'mysql://mysql:3306',
 	'options' => [
 		PDO::ATTR_PERSISTENT    => true,
     	PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION,
